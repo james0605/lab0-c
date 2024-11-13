f946ead302b21fa28622836b4bd5b1d5e06bef63
¨james0605¨ <j25839604@gmail.com>
2024-11-13 17:07:26 +0800

    Update q_new and q_free
    q_new:
    Using malloc to allocated the list_head memory.
    If didn't allocated successfully then return.
    Else initialize the list head by INIT_LIST_HEAD then return head.
    
    q_free:
    If input is NULL then return.
    Using list_for_each_entry_safe to iterate the queue.
    Using list_del to delete list in entry and using q_release_element to
    release the element_t.
    Finally free head.

--- queue.c
+++ queue.c
@@ -14,11 +14,27 @@
 /* Create an empty queue */
 struct list_head *q_new()
 {
-    return NULL;
+    struct list_head *head = malloc(sizeof(struct list_head));
+    if (!head) {
+        free(head);
+        return NULL;
+    }
+    INIT_LIST_HEAD(head);
+    return head;
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *head) {}
+void q_free(struct list_head *head)
+{
+    if (!head)
+        return;
+    element_t *entry, *safe;
+    list_for_each_entry_safe (entry, safe, head, list) {
+        list_del(&entry->list);
+        q_release_element(entry);
+    }
+    free(head);
+}
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
